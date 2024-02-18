e42e5986f041c42219cd1b163e3ce93134b15e7c
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-02-18 15:23:08 +0530

    Containers
    Made Containers, added changes to Dashboard css.

---
+++ src/Components/Rectangles/Rectangles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Rectangles.css';
+import { Link } from 'react-router-dom';
+const Rectangle = () => {
+    return (
+        <div className="rectangle">
+          <h1>Dashboard</h1>
+          <p>Welcome to the Dashboard</p>
+        </div>
+    );
+  };
