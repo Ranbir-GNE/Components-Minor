e11436268e081ae9e075bb5913885a8c863dc2f4
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-03-16 15:03:41 +0530

    Update LoginForm.js

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -23,6 +23,9 @@ const LoginForm = () => {
       const response = await fetch('https://edconnect-nine.vercel.app/api/auth/login', {
         method: 'POST',
         headers: {
+          'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Headers': '*',
+                  'Access-Control-Allow-Credentials': 'true',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
