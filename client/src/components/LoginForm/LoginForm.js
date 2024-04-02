1a900738212d03130ee6ef7fb4356c8832fd3be3
gundeeps247 <gundeepsinghm@gmail.com>
2024-04-02 14:26:37 +0530

    Update LoginForm.js

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -99,7 +99,7 @@ function App() {
 	async function loginUser(event) {
 		event.preventDefault()
 
-		const response = await fetch('http://localhost:5000/api/login', {
+		const response = await fetch('https://edconnect-nine.vercel.app/api/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
