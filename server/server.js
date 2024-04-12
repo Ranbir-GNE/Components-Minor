b894aeb10945f7931d51340a90337b95887862c7
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-04-12 15:10:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@
 // // Start the server
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -39,56 +38,59 @@ const User = require('./models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
+// Allow requests from your frontend domain
+const allowedOrigins = ['https://edconnect-dashboard-blond.vercel.app/'];
 app.use(cors({
-  origin: 'https://ed-connect.vercel.app/',
+  origin: allowedOrigins,
   credentials: true,
 }));
+
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://gundeepsinghm:collegepassword@cluster0.rnnuthn.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/api/register', async (req, res) => {
-	console.log(req.body)
-	try {
-		const newPassword = await bcrypt.hash(req.body.password, 10)
-		await User.create({
-			name: req.body.name,
-			email: req.body.email,
-			password: newPassword,
-		})
-		res.json({ status: 'ok' })
-	} catch (err) {
-		res.json({ status: 'error', error: 'Duplicate email' })
-	}
+  console.log(req.body)
+  try {
+    const newPassword = await bcrypt.hash(req.body.password, 10)
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: newPassword,
+    })
+    res.json({ status: 'ok' })
+  } catch (err) {
+    res.json({ status: 'error', error: 'Duplicate email' })
+  }
 })
 
 app.post('/api/login', async (req, res) => {
-	const user = await User.findOne({
-		email: req.body.email,
-	})
+  const user = await User.findOne({
+    email: req.body.email,
+  })
 
-	if (!user) {
-		return { status: 'error', error: 'Invalid login' }
-	}
+  if (!user) {
+    return res.json({ status: 'error', error: 'Invalid login' })
+  }
 
-	const isPasswordValid = await bcrypt.compare(
-		req.body.password,
-		user.password
-	)
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  )
 
-	if (isPasswordValid) {
-		const token = jwt.sign(
-			{
-				name: user.name,
-				email: user.email,
-			},
-			'secret123'
-		)
+  if (isPasswordValid) {
+    const token = jwt.sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      'secret123'
+    )
 
-		return res.json({ status: 'ok', user: token })
-	} else {
-		return res.json({ status: 'error', user: false })
-	}
+    return res.json({ status: 'ok', user: token })
+  } else {
+    return res.json({ status: 'error', user: false })
+  }
 })
 
 const authRoutes = require('./routes/auth');
